d96aac5bd4f460dc2dd2c9d09080b74c77893053
Kathryn Bennett <kbennett13@college.harvard.edu>
2015-01-28 23:54:39 -0500

    Add scatterplot and mouseover. Add option for years.

--- nfl_d3.js
+++ nfl_d3.js
@@ -1,21 +1,114 @@
 var data;
+var margin = {top: 20, right: 25, bottom: 20, left: 50};
+var width = window.innerWidth - document.getElementById("viewDiv").offsetWidth - document.getElementById("optionsDiv").offsetWidth - margin.left - margin.right;
+var scatterplotHeight = 0.5*window.innerHeight - margin.top - margin.bottom;
+var graphHeight = 0.5*window.innerHeight - margin.top - margin.bottom;
+
+var graphVisible = false;
 
 // d3
-d3.csv("data/passingData.csv", function(error, contents) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(contents);
-    data = contents;
-  }
-  
-  loaded();
-  buildScatterplot();
-});
+function plotStats(dataFile, limit) {
+  d3.csv(dataFile, function(error, contents) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(contents);
+      data = contents;
+    }
+    
+    buildScatterplot(limit);
+  });
+}
 
-function buildScatterplot() {
-  d3.select("body").selectAll("p")
-    .data(data).enter()
-    .append("p")
-    .text(function(d) { return d["Name"]; } ); //data.indexOf(d)
+function buildScatterplot(limit) {
+  //data.indexOf(d)
+  var xScale = d3.scale.linear();
+  var xMin = d3.min(data, function(d) {
+              return parseInt(d["Attempts"]);  //References first value in each subarray
+            });
+  var xMax = d3.max(data, function(d) {
+              return parseInt(d["Attempts"]);  //References first value in each subarray
+            });
+  xScale.domain([0,xMax]);
+  xScale.range([margin.left, width]);
+  var yScale = d3.scale.linear();
+  var yMin = d3.min(data, function(d) {
+                    return parseInt(d["Completions"]);  //References first value in each subarray
+                    });
+  var yMax = d3.max(data, function(d) {
+                    return parseInt(d["Completions"]);  //References first value in each subarray
+                    });
+  yScale.domain([0,yMax]);
+  yScale.range([scatterplotHeight, margin.bottom]);
+  
+  var svg = d3.select("#display")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", scatterplotHeight + margin.top + margin.bottom);
+  svg.selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function(d) {
+            return xScale(d["Attempts"]);
+            })
+      .attr("cy", function(d) {
+            return yScale(d["Completions"]);
+            })
+      .attr("r", 4)
+      .attr("fill", "red")
+      .attr("stroke", "black")
+      .attr("stroke-width",1)
+      .on("mouseenter", function(d) {
+        var xPosition = parseFloat(d3.select(this).attr("cx")) + parseInt(d3.select(this).attr("r")) + 1;
+        var yPosition = parseFloat(d3.select(this).attr("cy")) - parseInt(d3.select(this).attr("r")) - 1;
+          svg.append("text")
+          .attr("id", "tooltip")
+          .attr("x", xPosition)
+          .attr("y", yPosition)
+          .attr("text-anchor", "middle")
+          .attr("font-family", "sans-serif")
+          .attr("font-size", "12px")
+          .attr("font-weight", "bold")
+          .attr("fill", "black")
+          .text(d["Name"]);
+      })
+      .on("mouseleave", function(d) {
+        d3.select("#tooltip").remove();
+      })
+      .on("click", function(d) {
+        if (graphVisible) {
+          addToGraph(parseInt(d["Id"]));
+        } else {
+          drawGraph();
+          addToGraph(parseInt(d["Id"]));
+        }
+      });
+  
+  var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom");
+  svg.append("g")
+  .attr("class", "axis")
+  .attr("transform", "translate(0," + scatterplotHeight + ")")
+  .call(xAxis);
+  var yAxis = d3.svg.axis()
+  .scale(yScale)
+  .orient("left");
+  svg.append("g")
+  .attr("class", "axis")
+  .attr("transform", "translate(" + margin.left + ", 0)")
+  .call(yAxis);
+}
+
+function drawGraph() {
+  
+}
+
+function addToGraph(id) {
+  
+}
+
+function removeGraph() {
+  
 }
--- options.js
+++ options.js
@@ -1,6 +1,11 @@
-var viewOptions = ["Select View", "View By Player", "Compare Players By Position"];
+var viewOptions = ["Select View", "Compare Players", "Compare Players By Position"];
 var positionOptions = ["Select Position","QB", "WR", "RB", "TE", "WR/RB/TE", "K", "DEF"];
 var numberOptions = ["Select Limit",5,10,25,50];
+var yearMin = 2002;
+var yearMax = 2014;
+var yearOptions = [];
+var files = ["data/passingData.csv"];
+var dataFile;
 
 // build select for view and position options
 function buildOptions(name, optionList) {
@@ -17,10 +22,19 @@ function buildOptions(name, optionList) {
   return results;
 }
 
+function generateYears() {
+  yearOptions.push("Select Year");
+  for (y = yearMin; y <= yearMax; y++) {
+    yearOptions.push(y);
+  }
+}
+
 // event listeners
-function loaded() {
+window.onload = function() {
   document.getElementById("view").innerHTML = buildOptions("viewSelect", viewOptions);
   document.getElementById("position").innerHTML = buildOptions("positionSelect", positionOptions);
+  generateYears();
+  document.getElementById("year").innerHTML = buildOptions("yearSelect", yearOptions);
   document.getElementById("number").innerHTML = buildOptions("numberSelect", numberOptions);
   
   document.getElementById("viewSelect").onchange = function () {
@@ -40,16 +54,45 @@ function loaded() {
   }
   
   document.getElementById("positionSelect").onchange = function () {
-    if(document.getElementById("positionSelect").value == 0) {
+    value = document.getElementById("positionSelect").value;
+    if(value == 0) {
       var toHide = document.getElementsByClassName("post-position");
       for (el = 0; el < toHide.length; el++) {
         toHide[el].style.display = "none";
       }
     } else {
+      if (value == 1) {
+        dataFile = files[0];
+        plotStats(dataFile, null);
+      }
       var toShow = document.getElementsByClassName("post-position");
       for (el = 0; el < toShow.length; el++) {
         toShow[el].style.display = "inline";
       }
+    }
+  }
+  
+  document.getElementById("yearSelect").onchange = function () {
+    if(document.getElementById("yearSelect").value == 0) {
+      var toHide = document.getElementsByClassName("post-year");
+      for (el = 0; el < toHide.length; el++) {
+        toHide[el].style.display = "none";
+      }
+    } else {
+      var toShow = document.getElementsByClassName("post-year");
+      for (el = 0; el < toShow.length; el++) {
+        toShow[el].style.display = "inline";
+      }
+    }
+  }
+  
+  document.getElementById("numberSelect").onchange = function () {
+    var value = document.getElementById("numberSelect").value;
+    if(value == 0) {
+      limit = null;
+      buildScatterplot(limit);
+    } else {
+      buildScatterplot(numberOptions[value]);
     }
   }
 }
